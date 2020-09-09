4e7fbcd585d8d1cbc40db5c4c04bb8c78e22e99e
Taisei-Noguchi <tarchan0320@icloud.com>
2020-09-09 22:11:19 +0900

    出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,15 +6,27 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
   end
 
   def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
 
   end
 
   def edit
 
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :detail, :price, :category_id, :condition_id, :delivery_fee_id, :shipping_area_id, :shipping_days_id, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,22 @@
 class Item < ApplicationRecord
-  has_many :memorys
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_many :memories
   belongs_to :user
+
+  has_one_attached :image
+
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :shipping_area
+  belongs_to_active_hash :shipping_days
+
+  validates :name, presence: true
+  validates :detail, presence: true
+  validates :price, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 }
+  validates :condition_id, presence: true, numericality: { other_than: 1 }
+  validates :delivery_fee_id, presence: true, numericality: { other_than: 1 }
+  validates :shipping_area_id, presence: true, numericality: { other_than: 1 }
+  validates :shipping_days_id, presence: true, numericality: { other_than: 1 }
 end
