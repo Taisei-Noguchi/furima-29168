d3a3d607ef4e738a2f2f3d464f5e5879df9464ce
Taisei-Noguchi <tarchan0320@icloud.com>
2020-09-11 22:09:28 +0900

    商品出品機能　修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,13 +11,17 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_area
   belongs_to_active_hash :shipping_days
 
-  validates :image, presence: true
-  validates :name, presence: true
-  validates :detail, presence: true
-  validates :price, presence: true, format: { with:/\A[0-9]+\z/},numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }
-  validates :category_id, presence: true, numericality: { other_than: 1 }
-  validates :condition_id, presence: true, numericality: { other_than: 1 }
-  validates :delivery_fee_id, presence: true, numericality: { other_than: 1 }
-  validates :shipping_area_id, presence: true, numericality: { other_than: 1 }
-  validates :shipping_days_id, presence: true, numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :detail
+    validates :price, format: { with:/\A[0-9]+\z/},numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :delivery_fee_id
+      validates :shipping_area_id
+      validates :shipping_days_id
+    end
+  end
 end
