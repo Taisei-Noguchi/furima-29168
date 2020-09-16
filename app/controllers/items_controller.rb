32b61e8686060e1a88c6c742b6b60ad227a45cf3
Taisei-Noguchi <tarchan0320@icloud.com>
2020-09-16 10:11:43 +0900

    show editを消す

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,14 +20,6 @@ class ItemsController < ApplicationController
 
   end
 
-  def show
-    
-  end
-
-  def edit
-    
-  end
-
   def update
     if @item.update(item_params)
       redirect_to root_path
