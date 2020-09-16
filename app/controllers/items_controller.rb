3d89b89a167eb84dda91ed06594cf9c7b1f0cf59
Taisei-Noguchi <tarchan0320@icloud.com>
2020-09-16 10:39:12 +0900

    destroyアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
@@ -27,6 +27,10 @@ class ItemsController < ApplicationController
       render :edit
     end
 
+  end
+
+  def destroy
+    tweet.destroy
   end
 
   private
