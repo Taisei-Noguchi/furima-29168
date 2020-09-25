7ce9247387543c47cac58bd6984ce1b83adf5ee4
Taisei-Noguchi <tarchan0320@icloud.com>
2020-09-25 15:37:45 +0900

    購入テストコード

--- spec/factories/street_addresses.rb
+++ spec/factories/street_addresses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :street_address do
     postal_code    {"123-4567"}
-    shipping_area  {"11"}
+    shipping_area  {11}
     city           {"大阪市"}
     address_detail {"123"}
     phone_number   {"12312341234"}
